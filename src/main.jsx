9021f7ff3b0066e0db75d697ca2f006912938b71
uzumaki <leo.adolfo.cabello@gmail.com>
2026-09-11 20:26:59 -0300

    Load editorial layout fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/ai.css'
 import './styles/editorial.css'
+import './styles/editorial-fixes.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
