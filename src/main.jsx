86d1f95656c4de800b8ed200f1150c8ad82f625d
uzumaki <leo.adolfo.cabello@gmail.com>
2026-09-13 11:41:31 -0300

    Load account sync styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import './styles/ai.css'
 import './styles/editorial.css'
 import './styles/editorial-fixes.css'
-import './styles/ios-print.css'
+import './styles/sync.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
