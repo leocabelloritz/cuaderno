0d72dcd8dc5c2aa2b95c140eec8ca802b58c38de
uzumaki <leo.adolfo.cabello@gmail.com>
2026-09-13 17:56:15 -0300

    Carga interacción refinada de tarjetas semanales

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './styles/editorial-fixes.css'
 import './styles/sync.css'
 import './styles/selected-look.css'
 import './styles/compact-interactions.css'
+import './styles/weekly-cards.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
